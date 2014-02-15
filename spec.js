0c8749176b0ae4b9ddfecd85ff464bf2c45fbfac
Mattias Petter Johansson <mpj@spotify.com>
2014-02-15 17:25:18 +0100

    duplication

--- spec.js
+++ spec.js
@@ -13,24 +13,22 @@ module.exports = function(bus) {
     return {
       told: function(expectedAddress, expectedMessage) {
         var isOk = false
+        var expectationLog = {
+          when: [whenAddress, whenMessage],
+          told: [expectedAddress, expectedMessage]
+        }
         bus.on(expectedAddress).then(function(out, deliveries) {
           var message = deliveries[expectedAddress]
           if (message === expectedMessage) {
             isOk = true
-            out('expectation-ok', {
-              when: [whenAddress, whenMessage],
-              told: [expectedAddress, expectedMessage]
-            })
+            out('expectation-ok', expectationLog)
           }
         })
         bus.inject(whenAddress, whenMessage)
         return {
           check: function() {
             if(!isOk) {
-              bus.inject('expectation-failure', {
-                when: [whenAddress, whenMessage],
-                told: [expectedAddress, expectedMessage]
-              })
+              bus.inject('expectation-failure', expectationLog)
             }
           }
         }
